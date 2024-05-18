89aa519e64eb8ab9065e67e00a9de865535d939b
KartikayIITRpr <2020csb1094@iitrpr.ac.in>
2024-05-18 10:51:40 +0530

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Kartikay Singh on 18/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
